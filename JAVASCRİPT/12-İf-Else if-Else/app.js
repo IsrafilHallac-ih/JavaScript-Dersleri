ee3f3908d53db5b54c37346a4fef4228eb046f82
İSRAFİL HALLAÇ <83336393+IsrafilHallac-ih@users.noreply.github.com>
2023-11-20 21:30:32 +0300

    JavaScript
    if-else if-else

---
+++ JAVASCRİPT/12-İf-Else if-Else/app.js
@@ -0,0 +1,25 @@
+// --------Koşul Yapıları------
+
+let message=prompt("Merhaba Hoşgeldiniz..Nasılsınız?");
+
+if (message=="iyiyim") {
+    alert("İyi Olduğunuza Sevindim");
+    
+} else if(message=="kötüyüm") {
+    alert("Çok Üzüldüm");
+    
+}
+else if(message=="eh işte"){
+alert("Daha iyi olmanızı Temenni Ediyorum..");
+}else{
+    alert("Öyle Bi Cevap Yok..");
+}
+
+        
+    
+
+
+
+
+
+
