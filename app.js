bb4b825edeeb26e6120b42770d1f9544568e3127
İSRAFİL HALLAÇ <83336393+IsrafilHallac-ih@users.noreply.github.com>
2023-11-27 17:29:15 +0300

    JavaScript
    JS SWİTCH-CASE

---
+++ app.js
@@ -0,0 +1,35 @@
+
+let number=prompt("Lütfen 1 ile 5 arasında Sayı Giriniz");
+const message=("Girilen Sayı..:")
+switch (number) {
+    case "1":
+        //Kodlar
+         console.log(message + number);
+        break;
+    case "2":
+        console.log(message+number);
+        break;
+    case "3":
+        console.log(message+number);
+        break;
+    case "4":
+        console.log(message+number);
+        break;
+    case "5":
+        console.log(message+number);
+        break;      
+
+    default:
+        console.log(message+"1 ile 5 arasında değil..");
+        break;
+}
+ 
+
+        
+    
+
+
+
+
+
+
